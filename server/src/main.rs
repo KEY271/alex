9e02c3643268e36acb04a7dd0a01e48621297556
KEY271 <52455240+KEY271@users.noreply.github.com>
2024-08-30 02:31:09 +0900

    style

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,7 @@
 use std::{
-    env, str::FromStr, sync::{Arc, Mutex}
+    env,
+    str::FromStr,
+    sync::{Arc, Mutex},
 };
 
 use axum::{extract::State, http::HeaderValue, routing::get, Router};
@@ -23,15 +25,15 @@ async fn main() {
     }
     let board = Board::from_str("bngkpgnb/llhhhhll/8/8/8/8/LLHHHHLL/BNGPKGNB").unwrap();
     let state = Arc::new(Mutex::new(board));
-    let origins = [
-        "http://127.0.0.1:5173".parse::<HeaderValue>().unwrap()
-    ];
-    let app = app.with_state(state).layer(CorsLayer::new().allow_origin(origins));
+    let origins = ["http://127.0.0.1:5173".parse::<HeaderValue>().unwrap()];
+    let app = app
+        .with_state(state)
+        .layer(CorsLayer::new().allow_origin(origins));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3001")
         .await
         .unwrap();
 
-    println!("Server: http://localhost:3001");
+    println!("Server: http://127.0.0.1:3001");
     axum::serve(listener, app).await.unwrap();
 }
